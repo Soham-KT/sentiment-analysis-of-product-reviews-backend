1965ca22b4d082c01d6d71298c3a063d50cdc98d
Soham-KT <sohamkothariprof79@gmail.com>
2024-09-28 13:48:35 +0530

    hmmmmm

--- api/index.py
+++ api/index.py
@@ -5,6 +5,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
 @app.route('/data', methods=['GET'])
 def get_data():
     data = {
