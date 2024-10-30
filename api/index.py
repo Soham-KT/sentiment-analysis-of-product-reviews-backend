58c3b3c18d295dcde0914286e97ea41712e1fc9f
Soham-KT <sohamkothariprof79@gmail.com>
2024-10-30 18:36:56 +0530

    changes in main

--- api/index.py
+++ api/index.py
@@ -31,3 +31,6 @@ def predict():
     
     # return render_template('index.html', prediction_text = f'pred : {prediction}')
     return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True) 
