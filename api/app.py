ad178fc135ba8dc694358c46c725e02e56287798
Soham-KT <sohamkothariprof79@gmail.com>
2024-10-31 15:06:18 +0530

    serverless funtioon

--- api/app.py
+++ api/app.py
@@ -1,33 +1,51 @@
-from flask import Flask, request, jsonify
 from data_predict import model_predict
-from flask_cors import CORS
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import urllib.parse
 
-app = Flask(__name__)
-CORS(app)
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, World!')
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            
+            data = {
+                'Review': "This DVD will be a disappointment if you get it hoping to see some substantial portion of the acts of the various comics listed on the cover. All you get here are snippets of performance, at best. The rest is just loose-leaf reminiscence about the good old days in Boston, in the early 80's, when a lot of comics were hanging out together and getting their start. It's like a frat house reunion. There's a lot of lame nostalgia. There are quite a few guffaws recalling jokes (practical and otherwise) perpetrated - back then. But you had to have been there to appreciate all the basically good ol' boy camaraderie. If you weren't actually a part of that scene, all this joshing and jostling will fall flat. If you want to actually hear some of these comics' routines - you will have to look elsewhere.",
+                'prediction': 'negative',
+            }
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'404 Not Found')
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+    def do_POST(self):
+        if self.path == '/predict':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            parsed_data = urllib.parse.parse_qs(post_data.decode('utf-8'))
 
-@app.route('/data', methods=['GET'])
-def get_data():
-    data = {
-        'Review' : "This DVD will be a disappointment if you get it hoping to see some substantial portion of the acts of the various comics listed on the cover. All you get here are snippets of performance, at best. The rest is just loose-leaf reminiscence about the good old days in Boston, in the early 80's, when a lot of comics were hanging out together and getting their start.It's like a frat house reunion. There's a lot of lame nostalgia. There are quite a few guffaws recalling jokes (practical and otherwise)perpetrated - back then. But you had to have been there to appreciate all the basically good ol' boy camaraderie. If you weren't actually a part of that scene, all this joshing and jostling will fall flat.If you want to actually hear some of these comics' routines - you will have to look elsewhere.",
-        
-        'prediction': 'negative',
-    }
-    
-    return jsonify(data)
+            text = parsed_data.get('review', [None])[0]
+            prediction = model_predict(text=text)  # Ensure model_predict is defined/imported
 
-@app.route("/predict", methods=['POST'])
-def predict():
-    text = request.form['review']
-    prediction = model_predict(text=text)
-    
-    data = {
-        'review' : text,
-        'prediction': prediction
-    }
-    
-    # return render_template('index.html', prediction_text = f'pred : {prediction}')
-    return jsonify(data)
+            data = {
+                'review': text,
+                'prediction': prediction
+            }
+
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'404 Not Found')
